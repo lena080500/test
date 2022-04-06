1243bf91c7dc84926884aea2c618bfbc24befee4
lena080500 <lena89507369144@gmail.com>
2022-04-06 14:37:17 +0500

    обновил html файл (all..metrs)

--- testapps/urls.py
+++ testapps/urls.py
@@ -20,6 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cases/', include('cases.urls')),
-    path('', include('cases.urls'))
+    path('cases/alldateparametrs', include('cases.urls')),
 ]
