c887e84106afbf5400518d854bc393a736c249a7
Melkorz <djeday2000a@yandex.ru>
2022-04-06 14:29:58 +0500

    test

--- testapps/urls.py
+++ testapps/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cases/', include('cases.urls'))
-
+    path('cases/', include('cases.urls')),
+    path('', include('cases.urls'))
 ]
